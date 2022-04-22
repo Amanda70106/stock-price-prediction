d0ebdac28adb4b64502fa423ca7b7fcc0822985f
U08410006 <55177753+U08410006@users.noreply.github.com>
2022-04-22 22:19:45 +0800

    final one

--- decisionTree.py
+++ decisionTree.py
@@ -9,7 +9,10 @@ from sklearn import tree
 from sklearn.model_selection import cross_val_score
 from sklearn import metrics
 
-df = pd.read_csv('1216.csv')
+df = pd.read_csv('2330.csv')
+df = df.drop(['data','diff'], axis=1)
+print(df)
+
 CurrentCustomers = df.head(741)
 NewCustomers = df.tail(250)
 attributes = CurrentCustomers.drop('result',axis=1)
@@ -18,14 +21,14 @@ label = CurrentCustomers['result']
 DT = DecisionTreeClassifier(criterion='entropy')
 #print(DT)
 
-scores = cross_val_score(DT, attributes, label, cv=10, scoring='f1_macro',n_jobs=1)
+scores = cross_val_score(DT, attributes, label, cv=7, scoring='f1_macro',n_jobs=1)
 #print(scores)
 #print("F-score: %0.2f (+/= % 0.2f)" % (scores.mean(),scores.std()*2))
 
 DT_Model = DT.fit(attributes,label)
 
-feature_names = attributes.columns[:9]
-fig = plt.figure(figsize=(500,2000))
+feature_names = attributes.columns[:7]
+fig = plt.figure(figsize=(50,200))
 _ = tree.plot_tree(DT_Model, feature_names=feature_names, class_names='default.payment', filled = True)
 
 
