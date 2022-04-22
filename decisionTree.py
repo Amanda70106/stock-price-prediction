6647167b15e9371d5104fd462364522d0e398d50
Cheng Tsai Ling <wendyanime12@gmail.com>
2022-04-22 22:02:00 +0800

    add csv file

--- decisionTree.py
+++ decisionTree.py
@@ -1,6 +1,7 @@
 import matplotlib
 import numpy as np
 import pandas as pd
+from sklearn.metrics import classification_report, confusion_matrix
 from matplotlib import pyplot as plt
 
 from sklearn.tree import DecisionTreeClassifier 
@@ -8,31 +9,30 @@ from sklearn import tree
 from sklearn.model_selection import cross_val_score
 from sklearn import metrics
 
-df = pd.read_csv('UCI_Credit_Card.csv')
-CurrentCustomers = df.head(25000)
-NewCustomers = df.tail(5000)
-attributes = CurrentCustomers.drop('default.payment.next.month',axis=1)
-label = CurrentCustomers['default.payment.next.month']
+df = pd.read_csv('1216.csv')
+CurrentCustomers = df.head(741)
+NewCustomers = df.tail(250)
+attributes = CurrentCustomers.drop('result',axis=1)
+label = CurrentCustomers['result']
 
 DT = DecisionTreeClassifier(criterion='entropy')
-print(DT)
+#print(DT)
 
-scores = cross_val_score(DT, attributes, label, cv=10, scoring='f1_macro',n_jobs=10)
-print(scores)
-print("F-score: %0.2f (+/= % 0.2f)" % (scores.mean(),scores.std()*2))
+scores = cross_val_score(DT, attributes, label, cv=10, scoring='f1_macro',n_jobs=1)
+#print(scores)
+#print("F-score: %0.2f (+/= % 0.2f)" % (scores.mean(),scores.std()*2))
 
 DT_Model = DT.fit(attributes,label)
 
-feature_names = attributes.columns[:24]
-fig = plt.figure(figsize=(25,20))
+feature_names = attributes.columns[:9]
+fig = plt.figure(figsize=(500,2000))
 _ = tree.plot_tree(DT_Model, feature_names=feature_names, class_names='default.payment', filled = True)
 
 
-test_attributes = NewCustomers.drop('default.payment.next.month',axis=1)
-test_label = NewCustomers['default.payment.next.month']
+test_attributes = NewCustomers.drop('result',axis=1)
+test_label = NewCustomers['result']
 y_prediction = DT_Model.predict(test_attributes)
 
-from sklearn.metrics import classification_report, confusion_matrix
 print(confusion_matrix(test_label,y_prediction))
 print(classification_report(test_label,y_prediction))
 
