6131ef12c0eebb98213851e5ad527df4b1c191b6
U08410006 <55177753+U08410006@users.noreply.github.com>
2022-05-23 23:06:48 +0800

    modify the csv file data

--- make_csv.py
+++ make_csv.py
@@ -1,7 +1,7 @@
 stock = input("input the stock number:")
 
-f1name = "csv/" + stock + "_all.csv"
-f2name = "csv/" + stock + "_all_2.csv"
+f1name = "csv/mid_product/" + stock + "_all.csv"
+f2name = "csv/mid_product/" + stock + "_all_2.csv"
 foutname = "csv/" + stock + ".csv"
 # f1name = str(stock) + "_all.csv"
 # f2name = str(stock) + "_all_2.csv"
@@ -20,5 +20,12 @@ f2lines = f2.readlines()
 for i in range(len(f2lines)):
     pos = f1lines[i].find(',')
     f.write(f1lines[i][:pos].replace('-','0'))
-    f.write(f2lines[i][10:-2].replace('-','0'))
+    tocken_num = len(f2lines[i].split(','))
+    if(tocken_num<6):
+        f.write(f2lines[i][10:-2].replace('-','0'))
+    else:
+        f.write(',')
+        tokens = f2lines[i].split(',')
+        f.write(tokens[1] + "," + tokens[3] + "," + tokens[4] + ",")
     f.write(f1lines[i][pos+1:])
+        
