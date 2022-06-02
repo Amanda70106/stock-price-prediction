2b453e23adecea1484b2c8fb04b1def8b1d7c545
U08410006 <55177753+U08410006@users.noreply.github.com>
2022-06-02 23:07:47 +0800

    owo

--- ANN/ann.py
+++ ANN/ann.py
@@ -14,7 +14,9 @@ def normalize(data):
 def denormalize(original_data, scaled_data):
   denorm = scaled_data.apply(lambda x: x*(np.max(original_data)-np.min(original_data))+np.min(original_data))
   return denorm
-  
+
+stock_list = [1210,1231,2344,2449,2603,2633,3596,1215,1232,2345,2454,2607,2634,3682,1216,1434,2379,2455,2609,2637,4904,1218,1702,2408,2459,2610,3034,5388,1227,2330,2412,2468,2615,3035,1229,2337,2439,2498,2618,3045]
+
 filename = input('Input the csv file name: ')
 input_directory = os.path.abspath("../csv") + '/'
 datasets = pd.read_csv(input_directory + filename)
