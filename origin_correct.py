0b68c02628dac8323b1fb2a1b8e9f435ed0e3b11
U08410006 <55177753+U08410006@users.noreply.github.com>
2022-06-02 21:21:02 +0800

    add csv

--- origin_correct.py
+++ origin_correct.py
@@ -1,40 +1,36 @@
 stock = input("input the stock name:")
-stock_pos = "csv/" + stock + "_.csv"
+stock_pos = "csv/" + stock + "_all.csv"
 f = open(stock_pos,'r',encoding='utf-8')
-# f = open("2454_test2.csv",'r',encoding='utf-8')
 
 lines = f.readline()
 lines = f.readlines()
 line_cnt = 0
-float_set = [4,5,6,7,11,12,13,14,15]
-int_set = [2,3,9,10]
+float_set = [3,4,5,6]
+int_set = [1,2,8]
 error_cnt = 0
 for line in lines:
     line_cnt += 1 
     tokens = line.split(',')
-    if(len(tokens)!=17):
+    if(len(tokens)!=10):
         print("error tokens number in lines " + str(line_cnt))
         error_cnt += 1
-    dates = tokens[1].split('/')
+    dates = tokens[0].split('/')
     for date in dates:
         if(not date.isdigit()):
             print("error date in lines " + str(line_cnt))
             error_cnt += 1
     for i in float_set:
-        if(not tokens[i].replace('.','').replace('\n','').isdigit()):
+        if(not tokens[i].replace('.','').isdigit()):
             print("error " + str(i) + "th data in lines " + str(line_cnt))
-            # print(tokens[i].replace('.',''))
-            # print(type(tokens[i].replace('.','')))
-            # print(tokens[i].replace('.','').isdigit())
             error_cnt += 1
     for i in int_set:
         if(not tokens[i].isdigit()):
             print("error " + str(i) + "th data in lines " + str(line_cnt))
             error_cnt += 1
-    # if(not (tokens[13].replace('\n','') == "1" or tokens[12].replace('\n','') == "0")):
-    #     print("error at result in lines " + str(line_cnt))
-    #     error_cnt += 1
+    if(not (tokens[9].replace('\n','') == "1" or tokens[9].replace('\n','') == "0")):
+        print("error at result in lines " + str(line_cnt))
+        error_cnt += 1
 if(error_cnt == 0):
     print("this csv file is correct")
 else:
-    print("this csv file has " + str(error_cnt) + "errors.")
+    print("tish csv file has " + str(error_cnt) + "errors.")
