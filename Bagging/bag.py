283a904d0b4756ba6151505ef516e45d5edb3e5c
Amanda <alice70106@gmail.com>
2022-06-05 21:02:38 +0800

    csv

--- Bagging/bag.py
+++ Bagging/bag.py
@@ -12,7 +12,47 @@ import math
 def mathew(original, predict):
     tn, fp, fn, tp = confusion_matrix(original,predict).ravel()
     return (tp*tn-fp*fn)/math.sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn))
-  
+def smoothCut(df,days):
+    #moving average
+    df['close']=df['close'].rolling(days).mean()
+    #drop empty value
+    df=df.drop(index=[0,1,2,3,4,5,6,7,8],axis=0)
+    #calculate diff
+    r, c = df.shape
+    for i in range(r-1):
+        df.iloc[i,8] =df.iloc[i+1,7]-df.iloc[i,7]
+    for i in range(r-1):
+        if df.iloc[i,8]>0:
+            df.iloc[i,10]=1
+        else:
+            df.iloc[i,10]=0
+    df.to_csv("middle.csv")
+    return df
+def profit(df,signal):#df is original input data (from csv file and make close more smooth)
+                                   #y_prediction is predicted data (numpy array)
+    #signal is numpy array
+    signal = pd.DataFrame(signal)
+    signal = signal.reset_index()
+    signal = signal.iloc[:,1]
+    df = df.tail(len(signal))
+    df = df.reset_index()
+    df = df['close']
+    current = signal.iloc[0]
+    money=0
+    previous=current
+    #print(signal)
+    #print(df)
+    for i in range(1,len(df)):# i->tomorrow
+        if current!=signal.iloc[i] and previous==signal.iloc[i]:
+            if current==0:#buy
+                money = money-df.iloc[i-1]#扣今天的錢
+                current=1
+            else:   #sell
+                money = money+df.iloc[i-1]
+                current=0
+        previous=signal.iloc[i]#前一天是要買還是賣
+    return money
+money = []
 stock_list = [1210,1231,2344,2449,2603,2633,3596,1215,1232,2345,2454,2607,2634,3682,1216,1434,2379,2455,2609,2637,4904,1218,1702,2408,2459,2610,3034,5388,1227,2330,2412,2468,2615,3035,1229,2337,2439,2498,2618,3045]
 for stock in stock_list:
   print(stock)
@@ -21,7 +61,8 @@ for stock in stock_list:
   csv_file = "../csv/pridiction_result/" + str(stock) + ".csv"
   df = pd.read_csv(input_file)
   f = open(output_file, 'w',encoding='utf-8')
-  
+  original = df
+  df = smoothCut(df,10)
   CurrentCustomers=df.head(2000)
   NewCustomers=df.tail(939)
 
@@ -34,25 +75,27 @@ for stock in stock_list:
   label=CurrentCustomers['result']
   model = BaggingClassifier(base_estimator=None,n_estimators=500,n_jobs=-1)
   n_score=cross_val_score(model,attributes,label,scoring='f1_macro',cv=10,n_jobs=-1,error_score='raise')
-  print(n_score)
+  #print(n_score)
   print('F-Score: %.3f (%.3f)'%(mean(n_score),std(n_score)))
   learned_model=model.fit(attributes,label)
   test_attributes = NewCustomers.drop(['X','data','diff','result'],axis=1)
-  original = test_attributes
   test_attributes = normalize(test_attributes)
   test_label=NewCustomers['result']
   y_prediction = learned_model.predict(test_attributes)
   from sklearn.metrics import classification_report,confusion_matrix
-  print(confusion_matrix(test_label,y_prediction))
-  print(classification_report(test_label,y_prediction))
+  #print(confusion_matrix(test_label,y_prediction))
+  #print(classification_report(test_label,y_prediction))
   m=mathew(test_label,y_prediction)
-  Predict_result = pd.DataFrame(original)
+  money.append(profit(original,y_prediction)) 
+  Predict_result = pd.DataFrame(original.tail(len(test_attributes)))
   Predict_result["Prediction_Result"] = y_prediction
   output_filename = "output/Bagging_" + str(stock) +".csv"
-  print(output_file)
   Predict_result.to_csv(output_filename,mode='w' ,header = True, index = False)
   f.write(str(confusion_matrix(test_label,y_prediction)))
   f.write('\n')
   f.write(str(classification_report(test_label,y_prediction)))
   f.write('\n')
   f.write("matthews: %.4f" % (m))
+d = {'stockID': stock_list, 'profit': money}
+result = pd.DataFrame(data=d)
+result.to_csv("output/profit.csv",header = True, index = False,mode='a')
--- RandomForest/rf.py
+++ RandomForest/rf.py
@@ -16,9 +16,49 @@ def normalize(data):
 def mathew(original, predict):
     tn, fp, fn, tp = confusion_matrix(original,predict).ravel()
     return (tp*tn-fp*fn)/math.sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn))
+def smoothCut(df,days):
+    #moving average
+    df['close']=df['close'].rolling(days).mean()
+    #drop empty value
+    df=df.drop(index=[0,1,2,3,4,5,6,7,8],axis=0)
+    #calculate diff
+    r, c = df.shape
+    for i in range(r-1):
+        df.iloc[i,8] =df.iloc[i+1,7]-df.iloc[i,7]
+    for i in range(r-1):
+        if df.iloc[i,8]>0:
+            df.iloc[i,10]=1
+        else:
+            df.iloc[i,10]=0
+    df.to_csv("middle.csv")
+    return df
+def profit(df,signal):#df is original input data (from csv file and make close more smooth)
+                                   #y_prediction is predicted data (numpy array)
+    #signal is numpy array
+    signal = pd.DataFrame(signal)
+    signal = signal.reset_index()
+    signal = signal.iloc[:,1]
+    df = df.tail(len(signal))
+    df = df.reset_index()
+    df = df['close']
+    current = signal.iloc[0]
+    money=0
+    previous=current
+    #print(signal)
+    #print(df)
+    for i in range(1,len(df)):# i->tomorrow
+        if current!=signal.iloc[i] and previous==signal.iloc[i]:
+            if current==0:#buy
+                money = money-df.iloc[i-1]#扣今天的錢
+                current=1
+            else:   #sell
+                money = money+df.iloc[i-1]
+                current=0
+        previous=signal.iloc[i]#前一天是要買還是賣
+    return money
+money = []
 
-
-stock_list = ["test"]#[1210,1231,2344,2449,2603,2633,3596,1215,1232,2345,2454,2607,2634,3682,1216,1434,2379,2455,2609,2637,4904,1218,1702,2408,2459,2610,3034,5388,1227,2330,2412,2468,2615,3035,1229,2337,2439,2498,2618,3045]
+stock_list = [1210,1231,2344,2449,2603,2633,3596,1215,1232,2345,2454,2607,2634,3682,1216,1434,2379,2455,2609,2637,4904,1218,1702,2408,2459,2610,3034,5388,1227,2330,2412,2468,2615,3035,1229,2337,2439,2498,2618,3045]
 for stock in stock_list:
   print(stock)
   input_file = "../csv/" + str(stock) + ".csv"
@@ -26,7 +66,8 @@ for stock in stock_list:
   csv_file = "../csv/pridiction_result/" + str(stock) + ".csv"
   df = pd.read_csv(input_file)
   f = open(output_file, 'w',encoding='utf-8')
-
+  original = df
+  df = smoothCut(df,10)
   CurrentCustomers=df.head(2900)
 
 
@@ -49,9 +90,10 @@ for stock in stock_list:
   #test_attributes = normalize(test_attributes)
   y_prediction = learned_model.predict(test_attributes)
   from sklearn.metrics import classification_report,confusion_matrix
-  print(confusion_matrix(test_label,y_prediction))
-  print(classification_report(test_label,y_prediction))
+  # print(confusion_matrix(test_label,y_prediction))
+  # print(classification_report(test_label,y_prediction))
   m=mathew(test_label,y_prediction)
+  money.append(profit(original,y_prediction))
   Predict_result = pd.DataFrame(original)
   output_filename = "output/RandomForest" + str(stock) +".csv"
   Predict_result.to_csv(output_filename,mode='a' ,header = True, index = False)
@@ -60,3 +102,6 @@ for stock in stock_list:
   f.write(str(classification_report(test_label,y_prediction)))
   f.write('\n')
   f.write("matthews: %.4f" % (m))
+d = {'stockID': stock_list, 'profit': money}
+result = pd.DataFrame(data=d)
+result.to_csv("output/profit.csv",header = True, index = False,mode='a')
